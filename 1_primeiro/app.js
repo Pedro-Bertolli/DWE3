447b941127e84a84eadb02e71846f6e9d4224c4f
Pedro-Bertolli <pedrohbg34@gmail.com>
2025-08-19 12:00:11 -0300

    Adicionando 1_primeiro

--- 1_primeiro/app.js
+++ 1_primeiro/app.js
@@ -1,15 +1,18 @@
-const express = require('express')
-require('dotenv').config();
+//@ Importa as bibliotecas
+const express = require("express")
+require("dotenv").config();
 
-const app = express(); 
+//@ Configura o servidor
+const app = express();
 const port = process.env.PORT;
 
-//@ Cria uma rota para o raiz
-app.get('/', (req, res) => {
-  res.send('Hello DW3!')
+//@ Cria uma rota para o endereço raiz.
+app.get("/", (req, res) => {
+  res.send("Hello DW3!")
 })
 
+//@ Inicia o servidor
 app.listen(port, () => {
-  console.log('Executando a aplicação ' , process.env.APP_NAME);
-  console.log('Example app listening on port ', port);
+  console.log("Executando a aplicação:" , process.env.APP_NAME);
+  console.log("Example app listening on port:", port);
 })
